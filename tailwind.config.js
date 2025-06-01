4f25b755161692ba38834e35b785ae098adc82a8
viv2711 <vksingh27112001@gmail.com>
2025-06-01 21:19:25 +0530

    add basic signup form component, reconfigured the project structure

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+// tailwind.config.js
+import {heroui} from "@heroui/theme"
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./node_modules/@heroui/theme/dist/components/(card|ripple).js",
+  ],
+  theme: {
+    extend: {},
+  },
+  darkMode: "class",
+  plugins: [heroui()],
+};
