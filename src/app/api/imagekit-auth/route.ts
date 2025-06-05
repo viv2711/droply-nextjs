f8eb011e08dc23ce9116b76461d87cd9f711785b
viv2711 <vksingh27112001@gmail.com>
2025-06-05 19:00:54 +0530

    add route for uploading file using imagekit

---
+++ src/app/api/imagekit-auth/route.ts
@@ -0,0 +1,25 @@
+import { NextResponse } from "next/server";
+import { auth } from "@clerk/nextjs/server";
+import ImageKit from "imagekit"
+
+
+
+var imagekit = new ImageKit({
+    publicKey : process.env.NEXT_PUBLIC_IMAGEKIT_PUBLIC_KEY!,
+    privateKey : process.env.IMAGEKIT_PRIVATE_KEY!,
+    urlEndpoint : process.env.NEXT_PUBLIC_IMAGEKIT_URL_ENDPOINT!
+});
+
+export async function GET(){
+    try {
+        const {userId} = await auth()
+        if(!userId){
+            return NextResponse.json({error: "Unauthorized"}, {status: 401})
+        }
+        const authParams = imagekit.getAuthenticationParameters()
+        return NextResponse.json(authParams)
+    } catch (error) {
+        return NextResponse.json({error: "Failed to generate authentication parameters for imageKit"}, {status: 500})
+    }
+}
+
---
+++ src/app/api/upload/route.ts
@@ -0,0 +1,40 @@
+import { auth } from "@clerk/nextjs/server";
+import { db } from "@/lib/db";
+import {files} from "@/lib/db/schema"
+import { NextResponse } from "next/server";
+
+export async function POST(res: NextResponse){
+    try {
+        const {userId} = await auth()
+        if(!userId){
+            return NextResponse.json({error: "Unauthorized"}, {status: 401})
+        }
+
+        const body = await req.json();
+        const {imagekit, userId: bodyUserId } = body;
+        if(bodyUserId !== userId){
+            return NextResponse.json({error: "Unauthorized"}, {status: 401})
+        }
+        if(!imagekit || !imagekit.url){
+            return NextResponse.json({error: "Invalid file upload data"}, {status: 401})
+        }
+        const fileData = {
+            name: imagekit.name || "untitled",
+            path: imagekit.filePath || `/droply/${userId}/${imagekit.name}`,
+            size: imagekit.size || 0,
+            type: imagekit.fileType || "image",
+            fileUrl: imagekit.url,
+            thumbnailUrl: imagekit.thumbnailUrl || null,
+            userId: userId,
+            parentId: null,
+            isFolder: false,
+            isStarred: false,
+            isTrash: false   
+        }
+
+       const newFile =  await db.insert(files).values(fileData).returning()
+       return NextResponse.json(newFile);
+    } catch (error) {
+        return NextResponse.json({error: "Failed to save image..."+ error}, {status: 500})
+    }
+}
